e9dbf1c231935c5be97085baad3fd56c27eed6bc
zeroway <M_O_Bz@163.com>
2020-05-15 19:35:09 +0800

    I3bar dock, qutebrowser as default, completion heigh and download remove timeout setting

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -12,7 +12,8 @@ c.fonts.default_size = "15pt"
 c.fonts.hints = "bold 15pt default_family"
 c.downloads.position = 'bottom'
 c.tabs.show = 'never'
-#c.downloads.remove_finished = 5000
+c.completion.height = '30%'
+c.downloads.remove_finished = 5000
 
 # view for flash content(www-plugins/adobe-flash)
 c.content.plugins = True
